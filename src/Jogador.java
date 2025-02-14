c1414ef62a4c82a87bf37fdea1337df81d15b246
Gerry-Junior <gerry.casilla17@gmail.com>
2025-02-14 15:36:10 -0300

    Cmt

--- src/Jogador.java
+++ src/Jogador.java
@@ -2,6 +2,7 @@ public class Jogador  {
  int numero = 0;
 
     public void jogador(){
+        //palite dos jogadores
         numero = (int) (Math.random()*10);
         System.out.println("Estou pensando em " + numero);
 
