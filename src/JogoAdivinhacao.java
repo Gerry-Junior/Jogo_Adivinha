b4e0b542aa469e447c3ef0a6fc4ce3624b3f314a
Gerry-Junior <gerry.casilla17@gmail.com>
2025-02-14 15:34:56 -0300

    Comentarios atualizados

--- src/JogoAdivinhacao.java
+++ src/JogoAdivinhacao.java
@@ -11,27 +11,29 @@ public class JogoAdivinhacao   {
         int jogadorP1;
         int jogadorP2;
         int jogadorP3;
-
+        // primeiramente coloca os palpite do jogador como false antes de verificar
         boolean jogadorUmEstaCerto= false;
         boolean jogadorDoisEstaCerto = false;
         boolean jogadorTresEstaCerto = false;
 
-        int palpiteNumero = (int)(Math.random()*10);// multiplica os numero ate 10 para verificar o palpite da maquina s
+        int palpiteNumero = (int)(Math.random()*10);// a maquina verifica aleatoriamente os numero de 0 a 9
         System.out.println("Estou pensando em um numero de 0 a 9...");
 
+        //inicio do laço de repetição até verificar se um jogador acertou
         while (true){
-            System.out.println("O numero que estou adivinhando é " + palpiteNumero);
+            System.out.println("O numero que estou adivinhando é " + palpiteNumero);//palpite da maquina
+            // chama a classe Jogador para verificar qual numero os jogadores estão pensando
             jogadorUm.jogador();
             jogadorDois.jogador();
             jogadorTres.jogador();
-
+            //jogadores dão o palpite
             jogadorP1 = jogadorUm.numero;
             System.out.println("O jogador um forneceu o palpite " + jogadorUmEstaCerto );
             jogadorP2 = jogadorDois.numero;
             System.out.println("O jogador dois forneceu o palpite " + jogadorDoisEstaCerto);
             jogadorP3 = jogadorTres.numero;
             System.out.println("O jogador tres forneceu o palpite " + jogadorTresEstaCerto);
-
+            //verifica se o papilte do jogador esta certo
             if (jogadorP1 == palpiteNumero){
                 jogadorUmEstaCerto = true;
             }
@@ -41,7 +43,7 @@ public class JogoAdivinhacao   {
             if (jogadorP3 == palpiteNumero){
                 jogadorTresEstaCerto = true;
             }
-
+            // se um jogador acerta o jogo abaca e verifica se cada jogador acertou ou nao
             if (jogadorUmEstaCerto || jogadorDoisEstaCerto || jogadorTresEstaCerto){
                 System.out.println("Termo um jogador vencedor!");
                 System.out.println("Jogador Um Acertou? " + jogadorUmEstaCerto);
